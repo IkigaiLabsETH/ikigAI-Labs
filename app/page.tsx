31163c6309de4b87e292691f2ea7a78ff9e59227
ILXYZ <ikigailabs@proton.me>
2023-08-01 14:49:31 +0200

    test axios on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import HomePage from "@/templates/HomePage";
+import HomePage from "@/templates/HomePage/index_OG";
 
 const Home: NextPage = () => {
     return <HomePage />;

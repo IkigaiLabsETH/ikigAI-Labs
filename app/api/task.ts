01c1698441165174b5862383b9ba05d17f396313
ILXYZ <ikigailabs@proton.me>
2023-08-02 12:09:06 +0200

    commit

--- app/api/task.ts
+++ app/api/task.ts
@@ -1,44 +1,65 @@
 import { useEffect, useState } from 'react';
 
-export default function Home() {
-  const [input, setInput] = useState('');
+
+
+// Custom React Hook function
+const useCallApi = (input: string) => {
   const [image, setImage] = useState('');
   const [audio, setAudio] = useState('');
   const [video, setVideo] = useState('');
   const [text, setText] = useState('');
 
-  async function callApi() {
-    try {
+  useEffect(() => {
+    async function fetchData() {
+      try {
       if (!input) return
-      setImage('')
-      setAudio('')
-      setVideo('')
-      setText('')
+      setImage('');
+      setAudio('');
+      setVideo('');
+      setText('');
+
       const response = await fetch('./api/', {
         method: "POST",
         body: JSON.stringify({
           query: input
-        })
-      })
+        }),
+      });
+
       const { data, type } = await response.json()
       console.log('data:', data)
       if (type === 'image') {
-        setImage(data[0])
+        setImage(data[0]);
       }
       if (type === 'video') {
-        setVideo(data[0])
+        setVideo(data[0]);
       }
       if (type === 'audio') {
-        setAudio(data)
+        setAudio(data);
       }
       if (type == 'text') {
-        setText(data)
+        setText(data);
       }
     } catch (err) {
       console.log('error:', err)
     }
-    useEffect(() => {
-      callApi(); // Call the API when the component mounts
-    }, []); // Empty dependency array ensures it runs only once on mount  
   }
+
+  fetchData();
+}, [input]); // Dependency array ensures it runs when "input" changes
+
+// Return any state variables or functions as needed
+return { image, audio, video, text };
+};
+
+export default function Home() {
+   const [input, setInput] = useState('');
+
+   // Use the custom hook here
+   const { image, audio, video, text } = useCallApi(input);
+
+   return (
+    
+      {/* Your component JSX code here */}
+
+  );
 }
